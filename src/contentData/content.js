0b0d0a8e28b05bd71148a97d5ba5c7658ecfb629
alibabamtk62 <alibabamtk62@gmail.com>
2025-02-26 09:30:33 +0800

    Add files via upload

---
+++ src/contentData/content.js
@@ -0,0 +1,48 @@
+const productData = [
+    {
+      id: 1,
+      name: "BOX 5G SPEED PRO",
+      image:
+        "https://i.ibb.co/1RF1Gfq/favicon.png",
+      productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY"],
+      rating: 5,
+      timeLeft: 27,
+      appLink: "https://panelvip.co.uk/appbox-release.apk"
+       },
+    {
+      id: 2,
+      name: "180G HIGH SPEED PRO MAX",
+      image:
+        "https://i.ibb.co/wwm20Z4/icon-icon.png",
+      productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+      rating: 5,
+      timeLeft: 24,
+      appLink: "https://panelvip.co.uk/180ghighspeedpromax-release.apk" 
+    },
+    {
+      id: 3,
+      name: "IPHONE SPEED PRO",
+      image:
+        "https://i.ibb.co/z7pfLQc/icon-icon.png",
+      productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+      rating: 5,
+      timeLeft: 24,
+      appLink: "https://panelvip.co.uk/iphone.apk"
+
+    }
+    // {
+    //   id: 4,
+    //   name: "SECURE TUNNEL",
+    //   image:
+    //     "https://i.postimg.cc/wMW7LWFC/keyhole-958517.png",
+    //   productType: "openBackHeadphones",
+    //   description: ["OVPN", "UDP", "V2RAY"],
+    //   rating: 5,
+    //   timeLeft: 24,
+    //   appLink: "https://tinyurl.com/StarLinkVPNnew-Apk"
+    // }
+  ];
+  export default productData;
---
+++ src/contentData/siteOption.js
@@ -0,0 +1,11 @@
+const siteOptionData = [
+    {
+      id: 1,
+      siteName: "BLOODLINE DEV",
+      image:
+        "https://i.ibb.co/1RF1Gfq/favicon.png",
+      description: "Experience a secure and reliable VPN service with enhanced privacy and performance.",
+     // siteLink: "https://starlinkbrand.xyz/login"
+    }
+  ];
+  export default siteOptionData;
