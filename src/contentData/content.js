b8b2fc990df6fbff36783a4b58570be5e693b5db
MTK APPLICATION <alibabamtk62@gmail.com>
2025-07-12 11:10:35 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -1,36 +1,36 @@
 const productData = [
     {
       id: 1,
-      name: "BOX 5G SPEED PRO",
+      name: "SUMSAG ULTRA",
       image:
         "https://i.ibb.co/1RF1Gfq/favicon.png",
       productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY"],
+      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
       rating: 5,
       timeLeft: 27,
-      appLink: "https://panelvip.co.uk/appbox-release.apk"
-       },
-    {
-      id: 2,
-      name: "180G HIGH SPEED PRO MAX",
-      image:
-        "https://i.ibb.co/wwm20Z4/icon-icon.png",
-      productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
-      rating: 5,
-      timeLeft: 24,
-      appLink: "https://panelvip.co.uk/180ghighspeedpromax-release.apk" 
-    },
-    {
-      id: 3,
-      name: "IPHONE SPEED PRO",
-      image:
-        "https://i.ibb.co/z7pfLQc/icon-icon.png",
-      productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
-      rating: 5,
-      timeLeft: 24,
-      appLink: "https://panelvip.co.uk/iphone.apk"
+      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/final-release.apk"
+     //  },
+   // {
+    //  id: 2,
+     // name: "180G HIGH SPEED PRO MAX",
+     // image:
+     //   "https://i.ibb.co/wwm20Z4/icon-icon.png",
+     // productType: "openBackHeadphones",
+     // description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+     // rating: 5,
+     // timeLeft: 24,
+     // appLink: "https://panelvip.co.uk/180ghighspeedpromax-release.apk" 
+   // },
+    //{
+   //   id: 3,
+   //   name: "IPHONE SPEED PRO",
+    //  image:
+    //    "https://i.ibb.co/z7pfLQc/icon-icon.png",
+   //   productType: "openBackHeadphones",
+   //   description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+   //   rating: 5,
+    //  timeLeft: 24,
+   //   appLink: "https://panelvip.co.uk/iphone.apk"
 
     }
     // {
